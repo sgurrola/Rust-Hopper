3266602b693ac9003ae5e27403f43326dc3ad2d6
sgurrola <124643885+sgurrola@users.noreply.github.com>
2024-03-31 15:05:00 -0500

    shooter is fully okay now

--- target/debug/deps/notan_test-a02788bcf39528f4.d
+++ target/debug/deps/notan_test-a02788bcf39528f4.d
@@ -1,6 +1,6 @@
-C:\Users\sandr\Rust-Hopper\target\debug\deps\libnotan_test-a02788bcf39528f4.rmeta: src\main.rs src\assets/andrewzoom.png src\assets/guy_idle1.png src\assets/guy_idle2.png src\assets/guy_idle3.png src\assets/guy_falling1.png src\assets/guy_falling2.png src\assets/guy_falling3.png src\assets/cat-basket.png src\assets/python_enemy.png
+C:\Users\sandr\Rust-Hopper\target\debug\deps\libnotan_test-a02788bcf39528f4.rmeta: src\main.rs src\assets/andrewzoom.png src\assets/guy_idle1.png src\assets/guy_idle2.png src\assets/guy_idle3.png src\assets/guy_falling1.png src\assets/guy_falling2.png src\assets/guy_falling3.png src\assets/cat-basket.png
 
-C:\Users\sandr\Rust-Hopper\target\debug\deps\notan_test-a02788bcf39528f4.d: src\main.rs src\assets/andrewzoom.png src\assets/guy_idle1.png src\assets/guy_idle2.png src\assets/guy_idle3.png src\assets/guy_falling1.png src\assets/guy_falling2.png src\assets/guy_falling3.png src\assets/cat-basket.png src\assets/python_enemy.png
+C:\Users\sandr\Rust-Hopper\target\debug\deps\notan_test-a02788bcf39528f4.d: src\main.rs src\assets/andrewzoom.png src\assets/guy_idle1.png src\assets/guy_idle2.png src\assets/guy_idle3.png src\assets/guy_falling1.png src\assets/guy_falling2.png src\assets/guy_falling3.png src\assets/cat-basket.png
 
 src\main.rs:
 src\assets/andrewzoom.png:
@@ -11,4 +11,3 @@ src\assets/guy_falling1.png:
 src\assets/guy_falling2.png:
 src\assets/guy_falling3.png:
 src\assets/cat-basket.png:
-src\assets/python_enemy.png:
